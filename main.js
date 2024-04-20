3a8a570af6c95d2d3f18e72a4fdfd6e34e96055f
olehsupertiktnt228 <156704528+olehsupertiktnt228@users.noreply.github.com>
2024-04-20 15:18:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function D (){
+    return num1, num2, num3;
+}
+let num1 = 1;
+let num2 = -4;
+let num3 =  -21;
+
+console.log((num2*num2)-(4*num1)*(num3))
+
+
+function x1 (){
+   
+}
+
+console.log(((-num2)+Math.sqrt((num2 *num2) -( 4 * num1) * (num3))) /(2*num1))
+
+
+function x2 (){
+    return num4;
+}
+
+console.log(((-num2)-Math.sqrt((num2 *num2) -( 4 * num1) * (num3))) /(2*num1))
